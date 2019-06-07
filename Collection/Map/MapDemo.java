c8f69726f7f90803244ac4a8a445b831aa7714dc
wangming <wangming@163.com>
2019-06-07 19:07:32 +0800

    TreeMap

--- Collection/Map/MapDemo.java
+++ Collection/Map/MapDemo.java
@@ -81,4 +81,23 @@ public class MapDemo {
 
     }
 
+    /**
+     * 可以根据key进行排序
+     */
+    @Test
+    public void treeMapDemo(){
+        TreeMap<Integer, String> map = new TreeMap<>();
+        map.put(1, "语文");
+        map.put(3, "英语");
+        map.put(2, "数学");
+        map.put(4, "政治");
+        map.put(5, "历史");
+        map.put(6, "地理");
+        map.put(7, "生物");
+        map.put(8, "化学");
+        for(Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
 }
